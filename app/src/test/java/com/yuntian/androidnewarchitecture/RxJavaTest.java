023b7a9d7be3a61dfa232cfc1b0c4703870d727e
yuntian <944610721@qq.com>
2018-12-03 00:03:30 +0800

    rxjava学习

---
+++ app/src/test/java/com/yuntian/androidnewarchitecture/RxJavaTest.java
@@ -0,0 +1,16 @@
+package com.yuntian.androidnewarchitecture;
+
+import org.junit.Test;
+
+import io.reactivex.Flowable;
+
+/**
+ * @link {https://github.com/ReactiveX/RxJava}
+ */
+public class RxJavaTest {
+
+    @Test
+    public void  test1(){
+        Flowable.just("Hello world").subscribe(System.out::println);
+    }
+}
